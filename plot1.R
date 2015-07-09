9a2cebda6b9e13b8ecdeccb00e6b31418db49413
Sergey Kurda <srgkurda@gmail.com>
2015-07-09 18:06:23 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,6 @@
+getwd()
+setwd("C:/Users/Sergey/Documents/R/coursera/Exploratory DA")
+
 # download and read the source file. Can be skipped if once done
 url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(url, destfile="source.zip")
